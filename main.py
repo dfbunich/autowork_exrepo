f8ae2b92c9e4a6a332b9154cc2dc3961d63658b7
dfbunich <dfbunich@wm.edu>
2026-01-28 13:49:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def main():
-    print("Hello from autowork-exrepo!")
+from src.data import make_data, save_data
+from src.viz import make_figure
+
+def main() -> None:
+    data = make_data()
+    save_data(data, 'test2.npy')
+    make_figure(data, 'test_figure.svg')
+    return None
 
 
 if __name__ == "__main__":
